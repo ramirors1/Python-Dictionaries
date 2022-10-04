1cef804f960a6aa1bd54c614d3f7f9e4281bddff
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-10-04 13:24:41 +0000

    Added more functionality

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.", 
-    "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something repeatedly."
+    "Function": "A piece of code that you can easily call over and over again."
 }
 
+#Adding items to the dictionary
+programming_dictionary["Loop"] = "The action of doing something repeatedly"
+
 #Retrieving items from a dictionary
-print(programming_dictionary["Loop"])
+#print(programming_dictionary)
+
+#Creating an empty dictionary
+empty_dictionary = {}
+
+#Wiping a dictionary
+#programming_dictionary = {}
+#print(programming_dictionary)
+
+#editing an item in a dictionary
+programming_dictionary["Bug"] = "A big boo boo"
+#print(programming_dictionary)
+
+#Looping through a dictionary
+for key in programming_dictionary:
+    print(programming_dictionary[key])
